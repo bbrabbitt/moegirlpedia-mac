4251bd23d5d7dd795d410c3fc9e6bbff4895432a
bbrabbit <soranomethods@yahoo.co.jp>
2015-09-24 02:09:36 +0900

    project create

---
+++ moegirlpedia-mac/moegirlpedia-mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  moegirlpedia-mac
+//
+//  Created by 香風智乃 on 9/24/15.
+//  Copyright © 2015 bbrabbit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ moegirlpedia-mac/moegirlpedia-mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  moegirlpedia-mac
+//
+//  Created by 香風智乃 on 9/24/15.
+//  Copyright © 2015 bbrabbit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
